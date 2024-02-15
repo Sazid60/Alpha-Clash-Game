afcbc01c6722ab27f11a3fdf226803a38e2e12c1
Sazid60 <shahnawazsazid60@gmail.com>
2024-02-15 11:39:01 +0600

    Final Game

--- scripts/alpha-clash.js
+++ scripts/alpha-clash.js
@@ -10,21 +10,128 @@
 //     PlaygroundSection .classList.remove('hidden')
 // }
 
-function continueGame(){
+function play() {
+    //Hie verything except Playground
+    hideElementById('home-screen');
+    showElementById('play-ground');
+    //akbar gam Oever howar por ekhane disi
+    hideElementById('final-score');
+    // evabe game over howar por button click kore abar game calu korte caile score & life reset hobe na so we have to do it
+
+    //resent score & life
+    setElementValueById('current-life', 5)
+    setElementValueById('current-Score', 0)
+    continueGame();
+
+}
+
+function continueGame() {
     //step-1 : Generate e random alphabet
     const alphabet = getARandomAlphabet();
+    console.log('Your Random Alphabet :', alphabet)
     // console.log(alphabet);
 
     const currentAlphabetElement = document.getElementById('current-alphabet');
     currentAlphabetElement.innerText = alphabet;
-    
+
+
     //set Randomly Generated Alphabet to The Screen
     addBackgroundById(alphabet);
 }
 
-function play(){
-    hideElementById('home-screen');
-    showElementById('play-ground');
-    continueGame();
+//pura document ar keyup dilam
+document.addEventListener('keyup', handleKeyboardButtonPress);
+//FUNCTION CALL ER PORE JODI () DEI TAHLE FUNCTION KHALI AKBAR CALL HOBE NA DILE Joto bar press totobar dekhabe
 
+
+function handleKeyboardButtonPress(event) {
+    // console.log('button Pressed')
+    const playerPressed = event.key;
+    // console.log('Player Pressed :', playerPressed)
+    //event.target.value use kori nai because eta kuno input field e na
+
+    //stop game
+    if(playerPressed === 'Escape'){
+        gameOver();
+    }
+
+    //get the expected Press
+    //continueGame e set korsi & ekhane get korlam 
+    const currentAlphabetElement = document.getElementById('current-alphabet');
+    const currentAlphabet = currentAlphabetElement.innerText;
+    const expectedAlphabet = currentAlphabet.toLowerCase();
+    console.log(playerPressed, expectedAlphabet)
+
+    //check matched or not
+    if (playerPressed === expectedAlphabet) {
+        console.log('You Have Got a pont');
+        // ---------------------------------------------------------------
+        // //Update Score:
+        // //1. get the current score
+        // const currentScoreElement = document.getElementById('current-Score');
+        // const currentScoreText = currentScoreElement.innerText;
+        // const currentScore = parseInt(currentScoreText);
+
+        // //2. increase score by 1
+        // const newScore = currentScore + 1;
+        // //3. show the updated score
+        // currentScoreElement.innerText = newScore;
+
+        //uporer kaj ta sSame kaj akta function diye korasye anbo
+        const currentScore = getElementValueById('current-Score');
+
+        const newScore = currentScore + 1;
+        // console.log(newScore)
+
+        //Setting New Score
+        setElementValueById('current-Score', newScore)
+
+
+        //Start a new Round
+        removeBackgroundById(expectedAlphabet);
+        continueGame();
+    }
+    else {
+        console.log('You Lost a Life');
+        //------------------------------------------------------------
+        //         //Update Life:
+        // //1. get the current life
+        // const currentLifeElement = document.getElementById('current-life');
+        // const currentLifeText = currentLifeElement.innerText;
+        // const currentLife = parseInt(currentLifeText);
+
+        // //2. decrease life by 1
+        // const newLife = currentLife - 1;
+        // //3. show the updated life
+        // currentLifeElement.innerText = newLife;
+        //-----------------------------------------------------------
+
+        //uporer kaj tta sasme vabe korbo olpoo line e same as it is but if er utlity function re use  korbpo ar ki
+
+        const currentLife = getElementValueById('current-life');
+        const newLife = currentLife - 1;
+        setElementValueById('current-life', newLife)
+
+        if(newLife === 0){
+            gameOver();
+        }
+    }
 }
+
+function gameOver(){
+    hideElementById('play-ground');
+    showElementById('final-score');
+
+    //update final Score
+    //1.get the final score
+    const lastScore = getElementValueById('current-Score');
+    setElementValueById('last-score', lastScore)
+
+    //clear the last selected alphabet highlight
+    const currentAlphabet = getElementTextById('current-alphabet');
+    removeBackgroundById(currentAlphabet)
+} 
+
+
+//game over
+
--- scripts/utility.js
+++ scripts/utility.js
@@ -1,28 +1,54 @@
-function hideElementById(elementId){
+function hideElementById(elementId) {
     const element = document.getElementById(elementId);
     element.classList.add('hidden')
 }
 
-function showElementById(elementId){
+function showElementById(elementId) {
     const element = document.getElementById(elementId);
     element.classList.remove('hidden')
 }
 
-function getARandomAlphabet(){
+function getARandomAlphabet() {
     //Create a alphabet Array
     const alphabetString = 'abcdefghijklmnopqrstuvwxyz';
     const alphabets = alphabetString.split('');
 
     //get a randomm index between 0-25
-    const randomNumber = Math.random()*25;
+    const randomNumber = Math.random() * 25;
     const randomIndex = Math.round(randomNumber);
     const alphabet = alphabets[randomIndex];
     return alphabet;
 }
 
 //set Background Color
-function addBackgroundById(alphabetElementId){
-const alphabetElement = document.getElementById(alphabetElementId);
-alphabetElement.classList.add('bg-orange-400');
+function addBackgroundById(alphabetElementId) {
+    const alphabetElement = document.getElementById(alphabetElementId);
+    alphabetElement.classList.add('bg-orange-400');
 
+}
+//remove Background Color 
+function removeBackgroundById(alphabetElementId) {
+    const alphabetElement = document.getElementById(alphabetElementId);
+    alphabetElement.classList.remove('bg-orange-400');
+
+}
+
+//for getting the score value
+function getElementValueById(elementId) {
+    const elementText = document.getElementById(elementId);
+    const elementValue = elementText.innerText;
+    const value = parseInt(elementValue);
+    return value;
+}
+//for  updating score value
+function setElementValueById(elementId, newScore) {
+    const elementText = document.getElementById(elementId);
+    elementText.innerText = newScore;
+}
+
+//getting element text by id
+function getElementTextById(elementId){
+const element = document.getElementById(elementId)
+const text = element.innerText;
+return text;
 }
